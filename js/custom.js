593917d0f347b6d368f5906f823585cce2980648
Navindu <navindushane@gmail.com>
2019-01-11 12:08:37 +0530

    add preloader and back to top button

---
+++ js/custom.js
@@ -0,0 +1,3 @@
+$(window).load(function(){
+    $('.preloader').fadeOut(3000); // set duration in brackets    
+  });
---
+++ js/smoothscroll.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.custom-navbar a, #home a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+    });
+});
