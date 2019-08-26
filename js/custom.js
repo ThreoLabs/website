8fc76f81087beb169b469a0b735b734728cf1302
dcjc29 <durekshawasala@gmail.com>
2019-08-26 18:32:37 +0530

    Latest Update

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,3 @@
-var loader;
-
-function loadNow(opacity) {
-    if (opacity <= 0) {
-        displayContent();
-    } else {
-        loader.style.opacity = opacity;
-        window.setTimeout(function() {
-            loadNow(opacity - 0.05)
-        }, 100);
-    }
-}
-
-function displayContent() {
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    loader = document.getElementById('preloader');
-    loadNow(1);
-})
+$(window).load(function(){
+    $('.preloader').fadeOut(3000); // set duration in brackets    
+  });
